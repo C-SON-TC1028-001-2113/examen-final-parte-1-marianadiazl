083819199d1c349539835a470ae5a7723821b6f2
marianadiazl <a01252731@itesm.mx>
2021-10-15 17:23:04 +0000

    m

--- assignments/22InversoDigitos/src/exercise.py
+++ assignments/22InversoDigitos/src/exercise.py
@@ -1,7 +1,15 @@
 def main():
     num = int(input("Enter a number: "))
     #escribe tu código abajo de esta línea
-    
+    if len(str(num))>6:
+        print('Too long')
+    else:
+        if int(num)<0:
+            num=str(num)[::-1]
+            num=f"-{str(num)[:-1]}"
+        else:
+            num=str(num)[::-1]
+        print(num)
 
 
 if __name__=='__main__':
